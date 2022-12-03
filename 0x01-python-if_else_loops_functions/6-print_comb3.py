695eaf2330a6d47f1d31b78701fe53bfcf64c8d9
Boomni Jonathan <rejoiceoye1@gmail.com>
2022-12-03 23:06:14 +0100

    getting better

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+for num1 in range(0, 9):
+    for num2 in range(0, 10):
+        if num1 < num2:
+            if num1 < 8:
+                print("{:d}{:d}".format(num1, num2), end=", ")
+            else:
+                print("{:d}{:d}".format(num1, num2),)
