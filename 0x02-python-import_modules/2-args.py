51d4ac3bda80755489489ca15d486db83610195a
Boomni Jonathan <rejoiceoye1@gmail.com>
2022-12-06 21:54:57 +0100

    good

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    nargs = len(sys.argv)
-    count = nargs - 1
-    print("{:d} arguments:".format(count))
-    for i in range(nargs):
+    nargs = len(sys.argv) - 1
+    if (nargs == 0):
+        print("{:d} arguments.".format(nargs))
+    elif (nargs == 1):
+        print("{:d} argument:".format(nargs))
+    else:
+        print("{:d} arguments:".format(nargs))
+    for i in range(len(sys.argv)):
         if (i == 0):
             continue
         print("{:d}: {:s}".format(i, sys.argv[i]))
