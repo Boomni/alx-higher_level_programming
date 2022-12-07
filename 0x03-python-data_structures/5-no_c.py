fbdc11410131086e04358e3d994f47e7584b7a3d
Boomni Jonathan <rejoiceoye1@gmail.com>
2022-12-07 09:52:21 +0100

    corrected

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    my_string.remove(c)
-    my_string.remove(C)
-    return (my_string)
+    string = ""
+    for letters in my_string:
+        if letters != 'c' and letters != 'C':
+            string += letters
+    return (string)
