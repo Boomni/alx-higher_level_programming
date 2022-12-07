1795a924bf560452fc4737d271f7fb1c3d293aa6
Boomni Jonathan <rejoiceoye1@gmail.com>
2022-12-07 15:19:02 +0100

    maximum

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if my_list == "":
+        return (None)
+    maximum = 0
+    for i in range(len(my_list)):
+        if my_list[i] > maximum:
+            maximum = my_list[i]
+    return (maximum)
