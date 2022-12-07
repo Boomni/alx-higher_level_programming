bfd1f890e4b44be0e015eb36fcb95c0a6b8003f6
Boomni Jonathan <rejoiceoye1@gmail.com>
2022-12-07 15:05:36 +0100

    yse

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -10,10 +10,5 @@ def add_tuple(tuple_a=(), tuple_b=()):
             tuple_b = 0, 0
         else:
             tuple_b = tuple_b[0], 0
-    result = tuple(map(lambda i, j: i + j, tuple_a, tuple_b))
-    """
-    or
     result = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
-
-    """
     return (result)
