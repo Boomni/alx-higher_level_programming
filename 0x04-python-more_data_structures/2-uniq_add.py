37e224acf6b01827f8c72e42d2d59e28ba7c4c27
Boomni Jonathan <rejoiceoye1@gmail.com>
2022-12-08 22:11:33 +0100

    yeah

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
     result = 0
-
+    if (my_list == []):
+        return (0)
     for i in my_list:
+        print(i)
         result += i
     return (result)
