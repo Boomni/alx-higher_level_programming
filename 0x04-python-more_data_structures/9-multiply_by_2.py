246af836d95dcbed6dfa6f22cab4647f6d3135ed
Boomni Jonathan <rejoiceoye1@gmail.com>
2022-12-10 05:54:30 +0100

    solving outstanding tasks: 9

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-def multiply_by_2(a_dictionary):
-    new = {}
-    for i in a_dictionary:
-        new[i] = a_dictionary[i] * 2
-        return (new)
+def max_integer(my_list=[]):
+    if not my_list:
+        return None
+    biggest = 0
+    for index in my_list:
+        if index > biggest:
+            biggest = index
+    return (biggest)
