c544b43bd73a1bdcc7e1ba0e510a636844397289
Boomni Jonathan <rejoiceoye1@gmail.com>
2022-12-19 20:54:40 +0100

    solved task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,12 +2,11 @@
 
 def safe_print_list(my_list=[], x=0):
     count = 0
-    for i in my_list:
-        count += 1
     try:
         for i in range(x):
             print(my_list[i], end='')
-        print()
+            count += 1
     except IndexError:
         pass
+    print()
     return min(x, count)
