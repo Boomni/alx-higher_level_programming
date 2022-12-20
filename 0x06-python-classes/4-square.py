c665608a1f535765cd896e032fef6cbe16ea12d5
Boomni Jonathan <rejoiceoye1@gmail.com>
2022-12-20 23:40:29 +0100

    solved

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,17 +1,31 @@
 #!/usr/bin/python3
 
+"""Define a class Square"""
+
+
 class Square:
+
+    """Represent a square"""
+
     def __init__(self, size=0):
-        # Set size as the value of the private instance attribute __size
+        """Initialize square set size as value of private instance attribute
+
+        Args:
+            size: private instance attribute, size of new square
+        """
+
         self.__size = size
 
     @property
     def size(self):
-        # Return the value of the private instance attribute __size
+        """Get and return the value of the private instance attribute __size"""
+
         return self.__size
 
     @size.setter
     def size(self, value):
+        """sets the size of square as value"""
+
         # Check if value is an integer
         if not isinstance(value, int):
             # If value is not an integer, raise a TypeError exception
@@ -24,6 +38,7 @@ class Square:
         self.__size = value
 
     def area(self):
+        """Returns the area of the square by multiplying the size by itself"""
+
         size = self.__size
-        # Calculate the area of the square by multiplying the size by itself
         return (size * size)
