6bf821107fb6fdb79d153154ac1ac0d87333508b
Boomni Jonathan <rejoiceoye1@gmail.com>
2022-12-20 23:01:35 +0100

    solved

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,3 +1,18 @@
-#!/usr/bin/python3
 class Square:
-    pass
+    """
+    Class that represents a square.
+
+    Attributes:
+        __size (int): The size of the square.
+
+    """
+
+    def __init__(self, size=0):
+        """
+        Initialize a new Square instance.
+
+        Args:
+            size (int, optional): The size of the square. Defaults to 0.
+
+        """
+        self.__size = size
