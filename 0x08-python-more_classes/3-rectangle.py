eea5a8f0e8a4e82fe3df5c1fec7b6b048bb00039
Boomni Jonathan <rejoiceoye1@gmail.com>
2023-01-03 22:24:59 +0100

    ok

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -52,6 +52,7 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return (0)
         return ((self.__width * 2) + (self.__height * 2))
+
     def __str__(self):
         """Return a string representation of the rectangle"""
         if self.width == 0 or self.height == 0:
