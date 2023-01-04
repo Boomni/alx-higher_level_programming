55816efb78aaaf7825675c1202231f6c533c9d46
Boomni Jonathan <rejoiceoye1@gmail.com>
2023-01-04 10:56:10 +0100

    unittest

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -25,30 +25,6 @@ class TestMaxInteger(unittest.TestCase):
         self.assertEqual(max_integer([-5, 3, 0, -8]), 3)
         self.assertEqual(max_integer([-5, -3, 0, -1 -8]), 0)
 
-    def test_validation(self):
-        # Test that matrix_mul() raises the correct exceptions for invalid input
-        with self.assertRaises(TypeError, msg='m_a must be a list'):
-            matrix_mul('invalid', [[1, 2], [3, 4]])
-        with self.assertRaises(TypeError, msg='m_a must be a list of lists'):
-            matrix_mul([1, 2], [[1, 2], [3, 4]])
-        with self.assertRaises(ValueError, msg='m_a can\'t be empty'):
-            matrix_mul([], [[1, 2], [3, 4]])
-        with self.assertRaises(TypeError, msg='m_a should contain only integers or floats'):
-            matrix_mul([['invalid']], [[1, 2], [3, 4]])
-        with self.assertRaises(TypeError, msg='each row of m_a must be of the same size'):
-            matrix_mul([[1, 2], [3]], [[1, 2], [3, 4]])
-        with self.assertRaises(TypeError, msg='m_b must be a list'):
-            matrix_mul([[1, 2], [3, 4]], 'invalid')
-        with self.assertRaises(TypeError, msg='m_b must be a list of lists'):
-            matrix_mul([[1, 2], [3, 4]], [1, 2])
-        with self.assertRaises(ValueError, msg='m_b can\'t be empty'):
-            matrix_mul([[1, 2], [3, 4]], [])
-        with self.assertRaises(TypeError, msg='m_b should contain only integers or floats'):
-            matrix_mul([[1, 2], [3, 4]], [['invalid']])
-        with self.assertRaises(TypeError, msg='each row of m_b must be of the same size'):
-            matrix_mul([[1, 2], [3, 4]], [[1, 2], [3]])
-        with self.assertRaises(ValueError, msg='m_a and m_b can\'t be multiplied'):
-            matrix_mul([[1, 2], [3, 4]], [[1], [2]])
 
 if __name__ == '__main__':
     unittest.main()
