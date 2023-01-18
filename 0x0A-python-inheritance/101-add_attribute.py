71398e6959aced9fa47bd6e7d0a7fa4efe0985db
Boomni Jonathan <rejoiceoye1@gmail.com>
2023-01-18 11:32:19 +0100

    attempted task 101

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -5,6 +5,7 @@
 def add_attribute(obj, attr, val):
     """Adds an attribute to a function if possible"""
 
+    
     if not hasattr(obj, '__setattr__'):
         raise TypeError("can't add new attribute")
     obj.__setattr__(attr, val)
