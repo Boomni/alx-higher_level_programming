39d317958201d0b1fdd2ce6ecd8a52cdebe79045
Boomni Jonathan <rejoiceoye1@gmail.com>
2023-01-18 11:12:10 +0100

    attempted task 11

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -7,10 +7,10 @@ class Square(Rectangle):
     """
     This class defines a Square that inherits from Rectangle (9-rectangle.py):
         Instantiation with size: def __init__(self, size)::
-            size must be private. No getter or setter
-            size must be a positive integer, validated by integer_validator
-        the area() method must be implemented
-        print() should print, and str() should return, the square description: [Square] <width>/<height>
+            size is private. No getter or setter
+            size is a positive integer, validated by integer_validator
+        the area() is implemented
+        print() prints, and str() returns, the square description
     """
 
     def __init__(self, size):
