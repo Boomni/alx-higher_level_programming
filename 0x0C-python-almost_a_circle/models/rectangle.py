596afd311e157df19195703e0f22bd49c4630f43
Boomni Jonathan <rejoiceoye1@gmail.com>
2023-01-21 23:04:03 +0100

    attempted task 6

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -103,3 +103,11 @@ class Rectangle(Base):
         """Update the class Rectangle by overriding the __str__ method"""
         message = "[Rectangle] ({}) {}/{} - {}/{}"
         return message.format(self.id, self.x, self.y, self.width, self.height)
+
+    def update(self, *args):
+        """Public methodthat assigns an argument to each attribute"""
+        if len(args) > 0: self.id = args[0]
+        if len(args) > 1: self.width = args[1]
+        if len(args) > 2: self.height = args[2]
+        if len(args) > 3: self.x = args[3]
+        if len(args) > 4: self.y = args[4]
