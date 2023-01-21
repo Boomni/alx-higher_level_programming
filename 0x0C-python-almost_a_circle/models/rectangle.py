9f878a7fd80a507b6645c5730543fe5753c9b71f
Boomni Jonathan <rejoiceoye1@gmail.com>
2023-01-21 23:08:12 +0100

    attempted task 6

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -106,8 +106,13 @@ class Rectangle(Base):
 
     def update(self, *args):
         """Public methodthat assigns an argument to each attribute"""
-        if len(args) > 0: self.id = args[0]
-        if len(args) > 1: self.width = args[1]
-        if len(args) > 2: self.height = args[2]
-        if len(args) > 3: self.x = args[3]
-        if len(args) > 4: self.y = args[4]
+        if len(args) > 0:
+            self.id = args[0]
+        if len(args) > 1:
+            self.width = args[1]
+        if len(args) > 2:
+            self.height = args[2]
+        if len(args) > 3:
+            self.x = args[3]
+        if len(args) > 4:
+            self.y = args[4]
