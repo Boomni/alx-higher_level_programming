6b948c3bf2b593bb88b82292ead90dee9d0aa3b7
Jonathan Boomni <rejoiceoye1@gmail.com>
2023-02-14 19:34:04 +0100

    models/rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -123,4 +123,5 @@ class Rectangle(Base):
             [Rectangle] (<id>) <x>/<y> - <width>/<height>
 
         """
-        return ("[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.__x, self.__y, self.__width, self.__height))
+        string = "[Rectangle] ({}) {}/{} - {}/{}"
+        return string.format(self.id, self.x, self.y, self.width, self.height)
