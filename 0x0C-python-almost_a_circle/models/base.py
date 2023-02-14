5bf8e100b45fce2d82d6ce9272c59a7e502da1f4
Jonathan Boomni <rejoiceoye1@gmail.com>
2023-02-14 17:35:05 +0100

    editted

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """Module for Base class"""
-import json
 
 class Base:
     """A base class for other classes to inherit from.
@@ -25,23 +24,3 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
-
-
-    def to_json_string(list_dictionaries):
-	""" This function serializes a list of inputs """
-        if list_dictionaries is None:
-            return None
-        return json.dumps(list_dictionaries)
-
-    def from_json_string(json_string):
-        """Return the deserialization of a JSON string.
-        Args:
-            json_string (str): A JSON str representation of a list of dicts.
-        Returns:
-            If json_string is None or empty - an empty list.
-            Otherwise - the Python list represented by json_string.
-        """
-        if json_string is None:
-	""" Deserialization """
-            return None
-        return json.loads(json_string)
