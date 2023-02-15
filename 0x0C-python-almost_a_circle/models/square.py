5d84769184d9b8641e092c2423d19967d9cdbd05
Jonathan Boomni <rejoiceoye1@gmail.com>
2023-02-15 11:15:22 +0100

    models/square.py

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -40,3 +40,38 @@ class Square(Rectangle):
         """Returns the string representation of square instance"""
         string = "[Square] ({}) {}/{} - {}"
         return string.format(self.id, self.x, self.y, self.size)
+
+    def update(self, *args, **kwargs):
+        """
+        Public method that assigns attributes:
+
+        Args:
+            *args is the list of arguments - no-keyworded arguments
+                1st argument should be the id attribute
+                2nd argument should be the size attribute
+                3rd argument should be the x attribute
+                4th argument should be the y attribute
+            **kwargs:
+                variable number of key/value pairs
+        Returns:
+            The return statement is handled by __str__ method
+        """
+        if args is not None and len(args) != 0:
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) > 1:
+                self.size = args[1]
+            if len(args) > 2:
+                self.x = args[2]
+            if len(args) > 3:
+                self.y = args[3]
+        else:
+            for key, value in kwargs.items():
+                if key == "id":
+                    self.id = value
+                if key == "size":
+                    self.size = value
+                if key == "x":
+                    self.x = value
+                if key == "y":
+                    self.y = value
