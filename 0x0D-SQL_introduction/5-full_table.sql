244d85b1daae2aba2ea875e914d032157ba07175
Jonathan Boomni <rejoiceoye1@gmail.com>
2023-03-14 22:46:56 +0100

    sql 6

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,4 @@
+--  script that prints the full description of the table first_table from the database hbtn_0c_0 in MySQL server.
+-- The database name will be passed as an argument of the mysql command
+SELECT * FROM first_table;
+
