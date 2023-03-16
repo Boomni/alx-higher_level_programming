f1fdeb407c093ab959c924f7c8a23c4f2e8253a4
Jonathan Boomni <rejoiceoye1@gmail.com>
2023-03-16 13:42:53 +0100

    only grant select privilegges

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -3,4 +3,4 @@
 -- user_0d_2 password is set to user_0d_2_pwd
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS user_0d_2 IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON *.* TO user_0d_2@localhost;
+GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
