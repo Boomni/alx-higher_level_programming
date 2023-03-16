54e4ac96790b57e2b6e5bce448845abe268c141a
Jonathan Boomni <rejoiceoye1@gmail.com>
2023-03-16 15:00:49 +0100

    try

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -2,5 +2,5 @@
 -- user_0d_2 has only SELECT privilege in the database hbtn_0d_2
 -- user_0d_2 password is set to user_0d_2_pwd
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-CREATE USER IF NOT EXISTS user_0d_2 IDENTIFIED BY 'user_0d_2_pwd';
+CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
