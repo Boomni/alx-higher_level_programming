3763a073c7d6df74bea84292e5d1716e7e7f35f5
Jonathan Boomni <rejoiceoye1@gmail.com>
2023-04-11 18:00:20 +0100

    attempted task1 and task0 on printing to console

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,1 +1,7 @@
-#!/usr/bin/env node
+#!/usr/bin/node
+if (process.argv.length === 2)
+	console.log("No argument");
+else if (process.argv.length === 3)
+	console.log("Argument found");
+else
+	console.log("Arguments found");
