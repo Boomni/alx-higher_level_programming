1a0e871573e9fa0f413de90b911621bef105aa7a
Jonathan Boomni <rejoiceoye1@gmail.com>
2023-04-11 17:43:20 +0100

    attempted task1 and task0 on printing to console

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const myvar1 = 'C is fun\n';
-const myvar2 = 'Python is cool\n';
+const myvar1 = 'C is fun';
+const myvar2 = 'Python is fun';
 const myvar3 = 'JavaScript is amazing';
 console.log(myvar1, myvar2, myvar3);
