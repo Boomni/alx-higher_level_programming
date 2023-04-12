a10ce54db39794e3142cd15a5d5a3ebe472169fc
Jonathan Boomni <rejoiceoye1@gmail.com>
2023-04-12 14:06:57 +0100

    solved the bugs using semi standard

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,12 +1,4 @@
 #!/usr/bin/node
-<<<<<<< HEAD
-if (process.argv.length === 2) {
-  console.log('No argument');
-} else if (process.argv.length === 3) {
-  console.log('Argument found');
-} else if (process.argv.length > 3) {
-  console.log('Arguments found');
-=======
 if (process.argv.length === 2)
 {
 console.log('No argument');
@@ -18,5 +10,4 @@ console.log('Argument found');
 else
 {
 console.log('Arguments found');
->>>>>>> refs/remotes/origin/main
 }
