7c617c9ca3ba3c075d578974eae6e2c4ffb7ea87
Jonathan Boomni <110749205+Boomni@users.noreply.github.com>
2023-04-12 16:54:51 +0100

    Update 7-multi_c.js

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -4,7 +4,7 @@ const x = parseInt(process.argv[2]);
 
 if (isNaN(x)) {
   console.log('Missing number of occurrences');
-} else {
+} else if (x > 0) {
   for (let i = 0; i < x; i++) {
     console.log('C is fun');
   }
