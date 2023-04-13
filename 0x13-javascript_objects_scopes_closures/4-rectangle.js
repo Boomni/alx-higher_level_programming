c0ba63171e5d6f06d0eceaa64fb9dce8214d3ea4
Jonathan Boomni <rejoiceoye1@gmail.com>
2023-04-13 01:53:59 +0100

    drew square and implimented rotate and double members

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,29 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if ((w = parseInt(w)) > 0 && (h = parseInt(h)) > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  rotate () {
+    let temp = this.height;
+    this.height = this.width;
+    this.width = temp;
+  }
+
+  double () {
+    this.height = this.height * 2;
+    this.width = this.width * 2;
+  }
+}
+
+module.exports = Rectangle;
