3593d4dad9ca4f0c470c5577e37247ca6aef3516
Jonathan Boomni <rejoiceoye1@gmail.com>
2023-04-13 01:13:56 +0100

    Wrote a script that defines a rectange with constructors height and width and f w or h is equal to 0 or not a positive integer, create an empty object

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w <= 1 || h <= 1) {
+      return {};
+    } else {
+      this.width = w;
+      this.height = h;
+    }
+  }
+}
+module.exports = Rectangle;
