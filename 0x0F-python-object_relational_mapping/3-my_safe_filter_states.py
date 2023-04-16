0a02d1e86b9a62dd92f41bd7a8b6383f4eaec009
Jonathan Boomni <rejoiceoye1@gmail.com>
2023-04-16 18:34:51 +0100

    Fixed pycodestyle and checker bugs

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Takes in arguments and displays all values in the states table
+of hbtn_0e_0_usa where name matches the argument.
+Safe from MySQL injections!
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(host='localhost',
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3],
+                         port=3306)
+    cursor = db.cursor()
+    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC"
+    cursor.execute(query.format(sys.argv[4]))
+    data = cursor.fetchall()
+    for row in data:
+        print(row)
+    cursor.close()
+    db.close()
