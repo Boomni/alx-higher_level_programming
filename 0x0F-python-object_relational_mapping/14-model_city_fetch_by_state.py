0b5d5fdbafdf2b3b5b16165e5400fcb067b3c691
Jonathan Boomni <rejoiceoye1@gmail.com>
2023-04-17 01:23:17 +0100

    Completed mandatory tasks on python sqlalchemy orm

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -11,13 +11,12 @@ from model_city import City
 if __name__ == '__main__':
     username = sys.argv[1]
     password = sys.argv[2]
-    db_name = sys.argv[3]
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
-                           format(username,
-                                  password,
-                                  db_name),
+    database = sys.argv[3]
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
+                           .format(username,
+                                   password,
+                                   database),
                            pool_pre_ping=True)
-
     session = Session(engine)
 
     cities = session.query(State.name, City.id, City.name).\
--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -10,6 +10,10 @@ class City(Base):
 
     __tablename__ = 'cities'
 
-    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    id = Column(Integer,
+                primary_key=True,
+                nullable=False,
+                autoincrement=True,
+                unique=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
