c5e048f459480b45e771ce8630406fc5521d6926
Jonathan Boomni <rejoiceoye1@gmail.com>
2023-04-27 22:02:54 +0100

    Wrote some curl shell script that uses DELETE method

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 # Takes in a URL, sends a GET request to the URL, and displays the body of the response
-
 RESPONSE=$(curl -sI "$1" | grep HTTP | awk '{print $2}')
 if [ "$RESPONSE" -eq 200 ]; then
 	curl -s "$1" -X GET
