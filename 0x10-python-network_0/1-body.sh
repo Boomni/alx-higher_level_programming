75eb93497b7b709a29387f48f2ec2cd9c9255a26
Jonathan Boomni <rejoiceoye1@gmail.com>
2023-04-27 21:55:13 +0100

    Wrote some curl scripts

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Takes in a URL, sends a GET request to the URL, and displays the body of the response
 
-STATUS=$(curl -s -w '%{http_code}' -o /dev/null "$1")
-if [ "$STATUS" -eq 200 ]; then
+RESPONSE=$(curl -sI "$1" | grep HTTP | awk '{print $2}')
+if [ "$RESPONSE" -eq 200 ]; then
 	curl -s "$1"
 fi
