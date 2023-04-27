b176b999dc0501234bbb0069bcc1dd37dd347b67
Jonathan Boomni <rejoiceoye1@gmail.com>
2023-04-27 19:42:52 +0100

    Wrote some curl scripts

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 # Takes in a URL, sends a request to that URL, and displays the size of the body of the response
 
-RESPONSE=$(curl -s -w '%{size_download}' -o /dev/null "$1")
-echo "$RESPONSE"
+curl -s -w '%{size_download}' -o /dev/null "$1"
