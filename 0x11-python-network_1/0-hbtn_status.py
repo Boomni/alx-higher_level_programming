894a3996333c2766edacb6a943c69f137428290e
Jonathan Boomni <rejoiceoye1@gmail.com>
2023-04-28 19:37:54 +0100

    Requests script

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,12 +6,12 @@ Fetches https://alx-intranet.hbtn.io/status
 """
 
 
-import urllib.request
-
 if __name__ == '__main__':
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as request:
-        response = request.read()
+    import urllib.request
+
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as r:
+        response = r.read()
         print("Body response:")
         print("\t- type: {}".format(type(response)))
         print("\t- content: {}".format(response))
-        print("\t- utf8 content: {}".format(response.decode('UTF-8')))
+        print("\t- utf8 content: {}".format(response.decode('utf-8')))
