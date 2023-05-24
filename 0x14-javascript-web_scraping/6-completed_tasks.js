7c8958d0f5153a494ef4d21332c4dc29135d17db
Jonathan Boomni <rejoiceoye1@gmail.com>
2023-05-24 22:39:15 +0100

    Update

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -4,8 +4,9 @@ const request = require('request');
 const url = process.args[2];
 
 request(url, function (error, response, body) {
-  if (error) console.log('error:', error);
-  else {
+  if (error) {
+    console.log('error:', error);
+  } else {
     const todos = JSON.parse(body);
     const dash = {};
     for (let i = 0; i < todos.length; i++) {
