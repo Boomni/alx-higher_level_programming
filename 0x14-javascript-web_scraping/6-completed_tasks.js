31dc5dccba60161900de6991b455ad6a6ce24ece
Jonathan Boomni <rejoiceoye1@gmail.com>
2023-05-24 22:42:44 +0100

    Update

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -6,7 +6,7 @@ const url = process.argv[2];
 request(url, function (error, response, body) {
   if (!error) {
     const todos = JSON.parse(body);
-    let completed = {};
+    const completed = {};
     todos.forEach((todo) => {
       if (todo.completed && completed[todo.userId] === undefined) {
         completed[todo.userId] = 1;
